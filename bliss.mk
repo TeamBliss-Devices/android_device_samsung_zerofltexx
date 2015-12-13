673b208b4db18afd639697857504b724708572ee
Erwan Leboucher <erwanleboucher@gmail.com>
2015-12-13 15:12:50 +0100

    blissify

--- bliss.mk
+++ bliss.mk
@@ -17,10 +17,10 @@
 $(call inherit-product, device/samsung/zerofltexx/full_zerofltexx.mk)
 
 # Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/bliss/config/nfc_enhanced.mk)
 
 # Inherit common CM phone.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 PRODUCT_DEVICE := zerofltexx
-PRODUCT_NAME := cm_zerofltexx
+PRODUCT_NAME := bliss_zerofltexx
